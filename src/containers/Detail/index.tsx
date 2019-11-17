eed33565931b9ebf81113354c520366d45ad04c7
Hale Deng <haledeng88@gmail.com>
2019-11-17 20:56:02 +0800

    refactor: use React.lazy instead of react-loadable

--- src/containers/Detail/index.tsx
+++ src/containers/Detail/index.tsx
@@ -1,1 +1,19 @@
-export { default } from './Detail'
+import React, { Suspense } from 'react'
+import styled from 'styled-components'
+
+const LazyDetail = React.lazy(() => import('./Detail'))
+
+const Load = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const SuspenseDetail = () => (
+  <Suspense fallback={<Load>正在加载...</Load>}>
+    <LazyDetail />
+  </Suspense>
+)
+
+export default SuspenseDetail
