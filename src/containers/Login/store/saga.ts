d7af208a00b19d2f028434ca0c7b8ec57ae32ffd
Hale Deng <haledeng88@gmail.com>
2019-11-17 21:32:50 +0800

    fix: remove useless param

--- src/containers/Login/store/saga.ts
+++ src/containers/Login/store/saga.ts
@@ -1,8 +1,8 @@
 import { put, takeEvery, call, select } from 'redux-saga/effects'
 import * as request from '../../../request'
-import { User, REQUEST_LOGIN, CHANGE_LOGIN_STATUS } from './types'
+import { REQUEST_LOGIN, CHANGE_LOGIN_STATUS } from './types'
 
-function* fetchLogin(user: User) {
+function* fetchLogin() {
   const username = yield select(state => state.login.user.username)
   const password = yield select(state => state.login.user.password)
   const { response, error } = yield call(request.login, {
@@ -20,6 +20,5 @@ function* fetchLogin(user: User) {
 }
 
 export default function*() {
-  // @ts-ignore
   yield takeEvery(REQUEST_LOGIN, fetchLogin)
 }
